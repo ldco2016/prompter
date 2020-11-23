0ac9749c8ae5ebcf6ba2eef421a5c291668feeb9
ldco2016 <renaissance.scholar2012@gmail.com>
2020-11-23 09:13:12 -0600

    completed prompter

--- example/main.dart
+++ example/main.dart
@@ -2,8 +2,12 @@ import 'package:prompter_dc/src/option.dart';
 import 'package:prompter_dc/src/prompter.dart';
 
 void main() {
-  var options = [
+  final options = [
     new Option('I want red', '#f00'),
     new Option('I want blue', '#00f')
   ];
+
+  final prompter = new Prompter();
+  final usersPick = prompter.ask('What color do you like?', options);
+  print(usersPick);
 }
